61e010318476e16a121a6d7280c6ebf057da510e
sunxiaohui <343195025@qq.com>
2017-04-29 10:56:26 +0800

    加入创建数据库表和配置

---
+++ app/src/main/java/com/douziweather/android/MyApplication.java
@@ -0,0 +1,10 @@
+package com.douziweather.android;
+
+        import org.litepal.LitePalApplication;
+
+/**
+ * Created by Administrator on 2017/4/29.
+ */
+
+public class MyApplication extends LitePalApplication {
+}
